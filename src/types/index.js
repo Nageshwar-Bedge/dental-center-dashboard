8940acee715967bff17c904c0f592dbb6dd74db9
nagesh bedge <nageshwarbedge17@gmail.com>
2025-07-05 20:18:17 +0530

    Define UserRoles and AppointmentStatus constants

---
+++ src/types/index.js
@@ -0,0 +1,11 @@
+export const UserRoles = {
+  ADMIN: 'Admin',
+  PATIENT: 'Patient'
+};
+
+export const AppointmentStatus = {
+  SCHEDULED: 'Scheduled',
+  IN_PROGRESS: 'In Progress',
+  COMPLETED: 'Completed',
+  CANCELLED: 'Cancelled'
+};
